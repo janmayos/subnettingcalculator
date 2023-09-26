93b7cb801af02f7826a28bfeb8fa8c66075c6a8e
janmayos <jangasca5@gmail.com>
2023-09-26 10:57:24 -0600

    Tabla subneteo clase c

--- GeneradorSubneteo.py
+++ GeneradorSubneteo.py
@@ -14,11 +14,33 @@ def get_bits_prestados(numredes,clase):
         if numredes <= pow(2,bits)-2:
             return bits
 
+
+
 def genera_tablasubeteo(clase,ip,bitsprestados):
-    listbits = getbinario(4)
-    
-    
-    return 
+    clasebits = {"A":"00000000.00000000.00000000","B":"00000000.00000000","C":"00000000"}
+    listbits = getbinario(bitsprestados)
+    print("IP: ",ip)
+    ipsplit = ip.split(".")
+    print(ipsplit)
+    listasegmentos = []
+    if clase == "C":
+        ultimo = []
+        ban = False
+        for combinacionesip in listbits:
+
+            print(combinacionesip + " :  "+str(binario_a_decimal(combinacionesip)))
+            if ban:
+                ultimo.append(binario_a_decimal(combinacionesip)-1)
+                listasegmentos.append(ultimo)
+                ultimo = [ipsplit[0],ipsplit[1],ipsplit[2],combinacionesip,binario_a_decimal(combinacionesip)]
+            else:
+                ultimo = [ipsplit[0],ipsplit[1],ipsplit[2],combinacionesip,binario_a_decimal(combinacionesip)]
+                ban = True
+        ultimo.append(255)
+        listasegmentos.append(ultimo)
+
+
+    return listasegmentos
 
 
 
@@ -32,8 +54,21 @@ def aux_binario(numdecimal,decimal):
         i = i+1
 
     return binario
-    
+
+def binario_a_decimal(binario):
+    posicion = 0
+    decimal = 0
+    # Invertir la cadena porque debemos recorrerla de derecha a izquierda
+    binario = binario[::-1]
+    for digito in binario:
+        # Elevar 2 a la posición actual
+        multiplicador = 2**posicion
+        decimal += int(digito) * multiplicador
+        posicion += 1
+    return decimal
+
 def getbinario(bitsprestados):
+    
     listabinario = []
     maxdecimal = pow(2,bitsprestados)
     print(maxdecimal)
@@ -43,9 +78,13 @@ def getbinario(bitsprestados):
         decimal = numdecimal
         aux__binario = str(aux_binario(numdecimal,decimal))
         for ceros in range(bitsprestados-len(aux__binario)):
-            aux__binario = "0"+aux__binario
+            aux__binario = "0"+aux__binario     
+        
+        for ceros in range(8-len(aux__binario)):
+            aux__binario = aux__binario+"0"
         listabinario.append(aux__binario)
     return listabinario
+
 dictclase = {
     "A" : (1,126,24,"1.0.0.0","126.0.0.0"),
     "B" : (128,191,"128.0.0.0","191.255.0.0"),
